c409a005d6b7c7d2b8bfb4b8f3fc78bba24f4c88
agentpietrucha <agentpietrucha@gmail.com>
2023-05-24 19:36:36 +0200

    Add responsive images for 'installSamples' method

--- ps_imageslider.php
+++ ps_imageslider.php
@@ -149,7 +149,9 @@ class Ps_ImageSlider extends Module implements WidgetInterface
                     . '&utm_campaign=back-office-' . Tools::strtoupper($this->context->language->iso_code)
                     . '&utm_content=' . (defined('_PS_HOST_MODE_') ? 'ondemand' : 'download');
                 $rtlSuffix = $language['is_rtl'] ? '_rtl' : '';
-                $slide->image[$language['id_lang']] = sprintf('sample-%d%s.jpg', $i, $rtlSuffix);
+                foreach ($this->image_types as $image_type) {
+                    $slide->{'image_' . $image_type}[$language['id_lang']] = sprintf('sample-%d%s.jpg', $i, $rtlSuffix);
+                }
             }
             $slide->add();
         }
